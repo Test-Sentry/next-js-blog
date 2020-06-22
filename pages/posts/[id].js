a7650b8da0f5a4e5f7f5b642c55a5a824b2640ce
Stephen Cefali <stephencefali@gmail.com>
2020-06-22 15:05:30 -0700

    update

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -26,7 +26,6 @@ export default function Post({ postData }) {
 export async function getStaticPaths() {
   const paths = getAllPostIds();
   //hello 
-  //adfsad
   return {
     paths,
     fallback: false,
