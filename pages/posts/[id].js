6af4a781aa2a2c8648462e96edeacb6253f55b6e
Stephen Cefali <stephencefali@gmail.com>
2020-06-23 11:24:00 -0700

    update

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -10,7 +10,7 @@ export default function Post({ postData }) {
     <Layout>
       <Head>
         <title>{postData.title}</title>
-        <div>ddddd dd</div>
+        <div>ddddd ddd</div>
       </Head>
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
